12d39ba329b3e1dfb06197e0e5b051bd30a6b2bb
mordim3r <valent1n.bazylev@gmai.com>
2026-06-09 14:35:24 +0300

    add

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,48 @@
 package com.epam.mjc.nio;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class FileReader {
 
-    public Profile getDataFromFile(File file) {
-        return new Profile();
+    public Profile getDataFromFile(File file){
+        Profile profile = new Profile();
+       // Path path = file.toPath();
+
+        try{
+            List<String> lines = Files.readAllLines(file.toPath());
+            String[]parts;
+            String key;
+            String value;
+           for (String i:lines){
+            parts =i.split(":\\s*",2);
+            key= parts[0];
+            value=parts[1];
+            switch (key){
+                case "Name": profile.setName(value);
+                break;
+                case "Age": profile.setAge(Integer.parseInt(value));
+                    break;
+                case "Email": profile.setEmail(value);
+                    break;
+                case "Phone": profile.setPhone(Long.parseLong(value));
+                    break;
+
+            }
+           }
+
+        } catch (IOException e) {
+            System.err.println("не удалось прочесть файл"+e.getMessage());
+        }
+
+
+
+        return profile;
     }
 }
